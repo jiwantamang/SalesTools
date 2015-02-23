53e3b1ef6b24359f407b1a009cac0bc54f612a1c
jiwan <jiwan@jiwan-LIFEBOOK-AH530>
2015-02-23 11:58:55 +0545

    Initial Checkin of sales tools

---
+++ Sales Tool/src/ca/demo/salestools/Main.java
@@ -0,0 +1,22 @@
+package ca.demo.salestools;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Hello Happy Sales People");
+		System.out.println("This app Shows Sales Data");
+	}
+
+}
